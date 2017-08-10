6ea9bf60feeae5e3f6c716f00e4ea24b4fde2bcf
pajuhy123 <pajuhy123@gmail.com>
2017-08-10 14:36:41 +0900

    blog.models.Post 를 생성/migration/admin 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
+#blog/model.py
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name ='제목', help_text='포스팅 제목을 설정해 주세요') #길이 제한이 있는 문자열
+    content = models.TextField(verbose_name='내용')             #길이 제한이 없는 문자열
+    created_at = models.DateTimeField(auto_now_add = True) # 생성 될 떄, 자동 저장
+    updated_at = models.DateTimeField(auto_now=True)       # 갱신 될 때, 자동 갱신
