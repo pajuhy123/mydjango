4e4c82e669035aef9c91ce2a1fd7e50cb9e657c8
pajuhy123 <pajuhy123@gmail.com>
2017-08-10 16:05:24 +0900

    blog.models.Post 필드에 lnglat/tags 필드를 추가

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,19 @@
 #blog/model.py
+import re
+from django.forms import ValidationError
+
 from django.db import models
 
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*), ([+-]?\d+\.?\d*)$', value):  #위도 경도 타입을 검사하는 
+        raise ValidationError('invalid lnglat_type')                  #Validator
+
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name ='제목', help_text='포스팅 제목을 설정해 주세요') #길이 제한이 있는 문자열
+    title = models.CharField(max_length=100, verbose_name ='제목', help_text='포스팅 제목을 설정해 주세요, 최대 100자 내로 써주세요') #길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')             #길이 제한이 없는 문자열
+    tags = models.CharField(max_length=100, blank= True)
+    lnglat = models.CharField(max_length=50, blank=True, 
+            validators=[lnglat_validator],
+            help_text='경도/위도 포맷으로 입력해 주세여')
     created_at = models.DateTimeField(auto_now_add = True) # 생성 될 떄, 자동 저장
     updated_at = models.DateTimeField(auto_now=True)       # 갱신 될 때, 자동 갱신
