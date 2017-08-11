c200cc8a0afd1218304d985063f25147057e58be
pajuhy123 <pajuhy123@gmail.com>
2017-08-11 17:09:56 +0900

    auth.User 모댈 사용시 베스트 설정,' settings.AUTH_USER_MODEL'

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 #accounts/models.py
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-   user = models.OneToOneField(User)    #FIXME: BAD CASE !!!
+   #user = models.OneToOneField(User)    #FIXME: BAD CASE !!!
+   user = models.OneToOneField(settings.AUTH_USER_MODEL) #best!!
    phone_number = models.CharField(max_length =20)
    address = models.CharField(max_length =50)
 
