bd2f1d0ebf30431bf8658fd0e512c6c18384fa11
pajuhy123 <pajuhy123@gmail.com>
2017-08-12 14:06:18 +0900

    url reverse 샘플 코드 추가

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.post_list),
-    url(r'^(?P<id>\d+)/$', views.post_detail),
+    url(r'^$',views.post_list, name='post_list'),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'), #url revrse 
+                                                                          #를 사용한다면 view 가 역으로 url을 계산
+                                                                          #url이 변경되어도 쉽게 대처 가능!
     ]
--- blog/views.py
+++ blog/views.py
@@ -17,5 +17,5 @@ def post_detail(request, id):
     #    post = Post.objects.get(id=id)
     #except Post.DoesNotExist:
     #   raise Http404
-    post = get_object_or_404(Post,id=id)
+    post = get_object_or_404(Post,id=id)  #위에 4가지 코드를 1줄로!
     return render(request, 'blog/post_detail.html', {'post': post})
