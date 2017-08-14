291826ac2d963db276c5b195ae764e18516f659c
pajuhy123 <pajuhy123@gmail.com>
2017-08-14 16:37:04 +0900

    imagekit 을 활용하여, 이미지가 업로드 될 떄, 썸네일 처리된 이미지만 저장

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.forms import ValidationError
 from django.core.urlresolvers import reverse
 from django.db import models
-from imagekit.models import ImageSpecField
+from imagekit.models import ImageSpecField,ProcessedImageField
 from imagekit.processors import Thumbnail
 
 def lnglat_validator(value):
@@ -21,14 +21,18 @@ class Post(models.Model):
     #author = models.CharField(max_length=20) #blank 옵션을 주지 않았으므로 필수 필드
     title = models.CharField(max_length=100, verbose_name ='제목', help_text='포스팅 제목을 설정해 주세요, 최대 100자 내로 써주세요') #길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')             #길이 제한이 없는 문자열
-
+    photo = ProcessedImageField(blank=True, upload_to ='blog/post/%Y/%m/%d',
+                            processors=[Thumbnail(300, 300)], # 처리할 작업목록
+                            format='JPEG',         # 최종 저장포맷
+                            options={'quality': 60})# 저장 옵션
+    '''
     photo = models.ImageField(blank=True, upload_to ='blog/post/%Y/%m/%d')
     photo_thumbnail = ImageSpecField( # settings.MEDIA_ROOT 내 CACHES/ 하위 경로에 생성
                      source='photo', # 원본 ImageField 명 
                       processors=[Thumbnail(300, 300)], # 처리할 작업목록 
                       format='JPEG', # 최종 저장포맷 
                      options={'quality': 60}) # 저장 옵션
-    
+    '''
     tags = models.CharField(max_length=100, blank= True)
     lnglat = models.CharField(max_length=50, blank=True, 
             validators=[lnglat_validator],
