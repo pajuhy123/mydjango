ece0b0dd267691e74b0ddd01b9282ab12cfda299
pajuhy123 <pajuhy123@gmail.com>
2017-08-15 00:12:32 +0900

    CBV : DetailView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views_cbv
 
 urlpatterns=[
     url(r'^$',views_cbv.post_list, name='post_list'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'), #url revrse를 사용한다면 view 가 역으로 url을 계산 url이 변경되어도 쉽게 대처 가능!
+    url(r'^(?P<pk>\d+)/$', views_cbv.post_detail, name='post_detail'), #url revrse를 사용한다면 view 가 역으로 url을 계산 url이 변경되어도 쉽게 대처 가능!
             
     url(r'^new/$', views.post_new, name='post_new'),   
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
